f86332cfe63296069e23493d621d50ae7e501f58
Vineethkolli <kz8413@srmist.edu.in>
2024-12-06 21:24:33 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures relative paths for assets
   build: {
-    outDir: "dist", // Matches Vercel's distDir
-    emptyOutDir: true, // Ensures clean builds
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });

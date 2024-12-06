df6d9510c4962932d4a412499426e92da06843dc
Vineethkolli <kz8413@srmist.edu.in>
2024-12-06 20:59:23 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Ensure the correct base path
+  build: {
+    outDir: 'dist', // Ensure the output directory matches your Vercel config
+  },
+});

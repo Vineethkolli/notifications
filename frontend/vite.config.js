e2738aa2cad5c6ce325c6e7dc8a653aecdc19e0d
Vineethkolli <kz8413@srmist.edu.in>
2024-12-11 21:41:58 +0530

    ios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,52 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.js",
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'NBK Youth',
+        short_name: 'NBK Youth',
+        description: 'NBK Youth Gangavaram',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.origin === 'https://nbkyouth.vercel.app',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+            },
+          },
+          {
+            urlPattern: ({ url }) => url.origin === location.origin,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'assets-cache',
+            },
+          },
+        ],
+      },
     }),
   ],
-  base: "/",
-  build: {
-    outDir: "dist",
-    target: ["es2015"],
-    polyfillDynamicImport: true,
-  },
-  server: {
-    strictPort: true,
-    host: true,
-    port: 3000,
-  },
 });
